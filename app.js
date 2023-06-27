f3add9688abcf77984fdd72b0d5b81a7afa2b866
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-06-27 12:58:14 -0500

    add option page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-
+let welcomePage = document.querySelector('.welcome-page');
+let optionPage = document.querySelector('.option-page');
 
 
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('.btn').onclick = () => {
+
+        document.querySelector('.welcome-page').style.display = 'none';
+        document.querySelector('.option-page').style.opacity = 1;
+    }
+    
 
 })
